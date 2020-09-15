f2d2c7514019219604c5a51355fffb3aaae8c720
ArturSamvelyan <yourcamelcase@gmail.com>
2020-09-15 15:16:05 +0300

    multithreaded server (using threading library)

--- lab1/server.py
+++ lab1/server.py
@@ -1,21 +1,22 @@
 import socket
+import threading
 
+def process_request(conn, addr):
+    print("connected client: ", addr)
+    with conn:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print(data.decode("utf8"))
 
+# with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) for UDP sockets
 with socket.socket() as sock:
     sock.bind(("", 10001))
     sock.listen()
 
     while True:
         conn, addr = sock.accept()
-        conn.settimeout(5)
-        with conn:
-            while True:
-                try:
-                    data = conn.recv(1024)
-                except socket.timeout:
-                    print("close connection by timeout")
-                    break
-                    
-                if not data:
-                    break
-                print(data.decode("utf8"))
+        th = threading.Thread(target=process_request,
+                                args=(conn, addr))
+        th.start()
